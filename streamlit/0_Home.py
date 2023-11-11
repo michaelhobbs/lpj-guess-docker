4127c75c1fb4956a3081d7ae6a60d47b17bdb104
Michael Hobbs <michael@hobbs.im>
2023-11-11 22:36:28 +0100

    feat: expand streamlit dashboard

--- streamlit/0_Home.py
+++ streamlit/0_Home.py
@@ -1,14 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-demo_df: pd.DataFrame = pd.read_csv('../out/cpool.out', delim_whitespace=True)
-demo_df = demo_df.rename(columns={"Lon": "lon", "Lat": "lat"}, errors="raise")
+df_gridlist = pd.read_csv('../runs/gridlist.txt', delimiter='\t', header=None, names=['lon', 'lat', 'Place', 'Category'])
 
 st.title('LPJ-GUESS')
 st.markdown("""
             You are viewing the results of the last run of LPJ-GUESS. Browse the different outputs in the side bar.
-
-            The grid cell locations used in the last run of LPJ-GUESS:
             """)
-# st.sidebar.title("Home")
-st.map(demo_df, zoom=3)
+st.header('Gridcell locations')
+
+st.map(df_gridlist, zoom=2)
--- streamlit/pages/1_🏊_Carbon_Pool.py
+++ streamlit/pages/1_🏊_Carbon_Pool.py
@@ -1,21 +1,44 @@
+#%% imports
 import streamlit as st
 import pandas as pd
 import altair as alt
 
-demo_df: pd.DataFrame = pd.read_csv('../out/cpool.out', delim_whitespace=True)
-demo_df = demo_df.rename(columns={"Lon": "lon", "Lat": "lat"}, errors="raise")
+#%% loading data
+# Load the data from the files into pandas dataframes
+df_cpool = pd.read_csv('../out/cpool.out', delim_whitespace=True)
+df_gridlist = pd.read_csv('../runs/gridlist.txt', delimiter='\t', header=None, names=['Lon', 'Lat', 'Place', 'Category'])
 
+# Creating a dictionary for mapping from gridlist.txt
+mapping_dict = {(row['Lon'], row['Lat']): row['Place'] for index, row in df_gridlist.iterrows()}
+
+# Function to map category based on Lon and Lat
+def map_category(row):
+    return mapping_dict.get((row['Lon'], row['Lat']))
+
+# Applying the function to cpool dataframe
+df_cpool['Place'] = df_cpool.apply(map_category, axis=1)
+
+#%% creating page
 st.title('Carbon Pool')
 st.markdown("""
             The carbon pool is composed of:
             - `VegC`: Carbon contained in living vegetation
             - `LitterC`: Carbon found in litter, such as dead leaves
             - `SoilC`: Carbon found in the soil, eg: once litter has been processed by worms
+
+            When comparing several gridcells, we use the `Total` carbon pool, sum of `VegC`, `LitterC` and `SoilC`.
             """)
-st.sidebar.markdown("# 🏊 Carbon Pool")
-# st.bar_chart(demo_df[['VegC', 'LitterC', 'SoilC']])
+
+
+st.header('Single Gridcell')
+single = st.selectbox(
+    'Gridcell to view composition',
+    df_cpool['Place'].unique(),
+    0)
+
+single_df = df_cpool[df_cpool['Place'] == single]
 chart = (
-    alt.Chart(demo_df[['Year', 'SoilC', 'LitterC', 'VegC']].melt('Year', var_name='Type', value_name='Amount'))
+    alt.Chart(single_df[['Year', 'SoilC', 'LitterC', 'VegC']].melt('Year', var_name='Type', value_name='Amount'))
     .mark_area(opacity=0.3)
     .encode(
         x="Year:Q",
@@ -26,6 +49,66 @@ chart = (
 )
 st.altair_chart(chart, use_container_width=True)
 
-if st.checkbox('Show raw data'):
+
+st.header('Multiple Gridcells')
+#%% selection of gridcells to vizualize
+options = st.multiselect(
+    'Gridcells to view',
+    df_cpool['Place'].unique(),
+    None)
+
+
+#%% lasagna plot
+color_condition = alt.condition(
+    'datum.value % 10 == 0',
+    alt.value("white"),
+    alt.value(None),
+)
+selected = df_cpool
+if len(options) != 0:
+    selected = df_cpool[df_cpool['Place'].isin(options)]
+df_cpool = selected
+lasagna = alt.Chart(df_cpool).mark_rect().encode(
+    alt.X("Year:O").axis(
+            labelAngle=0,
+            labelOverlap=False,
+            labelColor=color_condition,
+            tickColor=color_condition,
+        ),
+    alt.Y("Place:N").title(None),
+    alt.Color("Total:Q")
+)
+st.altair_chart(lasagna, use_container_width=True, theme=None)
+
+#%% Plotting Total Cpool by place per year
+# Create a common chart object
+chart = alt.Chart(df_cpool, height=600).encode(
+    alt.Color("Place").legend(orient='bottom', direction='vertical')
+)
+
+# Draw the line
+line = chart.mark_line().encode(
+    x="Year:Q",
+    y="Total:Q"
+)
+
+# Use the `argmax` aggregate to limit the dataset to the final value
+label = chart.encode(
+    x='max(Year):Q',
+    y=alt.Y('Total:Q').aggregate(argmax='Year'),
+    text='Place'
+)
+
+# Create a text label
+text = label.mark_text(align='left', dx=4)
+
+# Create a circle annotation
+circle = label.mark_circle()
+
+# Draw the chart with all the layers combined
+st.altair_chart(line + circle + text, use_container_width=True)
+
+#%% Raw data
+with st.expander('Show raw data'):
     st.subheader('Raw data')
-    st.dataframe(demo_df)
+    st.dataframe(df_cpool)
